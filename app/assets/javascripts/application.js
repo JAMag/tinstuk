b6e836390f1497e83c0715e04944ec3e662815c7
Justin Maguire <maguirejustin@hotmail.com>
2016-10-06 15:40:25 -0400

    User Model Created and App structure

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+= require turbolinks
+//= require bootstrap-sprockets
+//= require jquery.easing.min
+//= require jasny-bootstrap
+//= require supersized.3.2.7
 //= require_tree .
